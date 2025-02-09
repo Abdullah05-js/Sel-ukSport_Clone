cc731567c72adcf5cda53c7da29d726702c820bb
thodex <abdallhameed2005@gmail.com>
2025-02-09 16:26:09 +0300

    fixing adsense

--- frontend/src/componets/Ads/Ads.js
+++ frontend/src/componets/Ads/Ads.js
@@ -1,15 +1,14 @@
 import Script from "next/script";
 import React from "react";
-
 const Ads = () => {
-  return (
-    <Script
-      id="adsbygoogle-init"
-      async
-      strategy="afterInteractive"
-      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4434411269474085"
-    />
-  );
-};
+    return (
+        <Script
+       async 
+       src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4434411269474085"
+       crossOrigin="anonymous"
+       strategy="afterInteractive"
+        />
+    );
+}
 
 export default Ads;
