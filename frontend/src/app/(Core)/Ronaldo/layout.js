b4766151374a055b15f8bf49853712c51a6ac564
thodex <abdallhameed2005@gmail.com>
2025-03-14 23:58:27 +0300

    still testing 2

--- frontend/src/app/(Core)/Ronaldo/layout.js
+++ frontend/src/app/(Core)/Ronaldo/layout.js
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import Head from "next/head";
+
 import "../../globals.css";
 
 const geistSans = Geist({
@@ -15,24 +15,12 @@ const geistMono = Geist_Mono({
 
 
 
+
+
 export default async function Layout({ children }) {
 
     return (
         <html lang="en">
-
-            <Head>
-
-                <meta name="twitter:card" content="player" />
-                <meta name="twitter:title" content="Your Video Title" />
-                <meta name="twitter:description" content="A brief description of your video" />
-                <meta name="twitter:image" content="https://thodex.live/opengraph-image.png" />
-                <meta name="twitter:player" content="https://thodexlive.blogspot.com/2025/02/1.html" />
-                <meta name="twitter:player:width" content="1280" />
-                <meta name="twitter:player:height" content="720" />
-                <meta name="twitter:player:stream" content="https://yourwebsite.com/videos/videoplayback.mp4" />
-                <meta name="twitter:player:stream:content_type" content="video/mp4" />
-            </Head>
-
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased `}
             >
